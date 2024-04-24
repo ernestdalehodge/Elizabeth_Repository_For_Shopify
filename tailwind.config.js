d555640e4499c463a02f1bc2b1ecd742e38e16f0
Ernest Hodge <ErnestHodge@Yahoo.com>
2024-04-24 11:25:15 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templetes/customers/*.liquid',
+    './templetes/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
